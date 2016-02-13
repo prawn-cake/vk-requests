f33e6853481d5b8d48705b13179ceaeaf3ec0f1c
Maksim Ekimovskii <ekimovsky.maksim@gmail.com>
2016-02-14 01:21:06 +0800

    Improved create_api factory method, added session_cls parameter

--- README.md
+++ README.md
@@ -40,14 +40,13 @@ Just pass `scope` and/or `api_version` parameters like
 ### Interactive session. 
 
 Useful for dev purposes. You will be asked about login, password and app_id 
-interactively in console
+interactively in console. Useful if capture required.
         
         from vk_requests.api import API
         from vk_requests.auth import InteractiveVKSession
         
         
-        session = InteractiveVKSession()
-        api = API(session=session, timeout=10)
+        api = vk_requests.create_api(..., session_cls=InteractiveVKSession)
 
 
 ### Auto-resolving conflicts when you getting access from unusual place
--- vk_requests/__init__.py
+++ vk_requests/__init__.py
@@ -8,15 +8,25 @@ __version__ = '0.9.1'
 
 def create_api(app_id=None, login=None, password=None, phone_number=None,
                timeout=10, scope='offline', api_version=None,
-               **method_default_args):
+               session_cls=VKSession, **method_default_args):
     """Factory method to explicitly create API with app_id, login, password
     and phone_number parameters.
-    If it's not passed (app_id, login, password) - token-free session will be
-    created automatically
 
+    If the app_id, login, password are not passed, then token-free session
+    will be created automatically
+
+    :param app_id: int: vk application id, more info: https://vk.com/dev/main
+    :param login: str: vk login
+    :param password: str: vk password
+    :param phone_number: str: phone number with country code (+71234568990)
+    :param timeout: int: api timeout in seconds
+    :param scope: str or list of str: vk session scope
+    :param api_version: str: vk api version
+    :param session_cls: VKSession: session implementation class
+    :param method_default_args: api kwargs
     :return: api instance
     :rtype : vk_requests.api.API
     """
-    session = VKSession(app_id, login, password, phone_number=phone_number,
-                        scope=scope, api_version=api_version)
+    session = session_cls(app_id, login, password, phone_number=phone_number,
+                          scope=scope, api_version=api_version)
     return API(session=session, timeout=timeout, **method_default_args)
