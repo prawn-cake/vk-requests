f04c7e3df81596259bca1166760fed2a887b3b5c
Maksim Ekimovskii <ekimovsky.maksim@gmail.com>
2016-07-06 08:40:40 +0300

    'Untight' requirements versions, to prevent downgrading behavior in client apps

--- setup.py
+++ setup.py
@@ -11,8 +11,8 @@ setup(
     description='vk.com requests for humans. API library for vk.com',
     install_requires=[
         'six',
-        'requests==2.8.1',
-        'beautifulsoup4==4.4.1'
+        'requests>=2.8.1',
+        'beautifulsoup4>=4.4.1'
     ],
     classifiers=[
         'Intended Audience :: Developers',
