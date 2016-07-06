ec2fc206d889fa385b5a4cbb9daf078cef378afa
Maksim Ekimovskii <ekimovsky.maksim@gmail.com>
2016-07-06 17:24:31 +0300

    Bump version: 0.9.5 → 0.9.6

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='vk-requests',
-    version='0.9.5',
+    version='0.9.6',
     packages=['vk_requests'],
     url='https://github.com/prawn-cake/vk-requests',
     license='MIT',
