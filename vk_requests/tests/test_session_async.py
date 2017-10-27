164161a822eab98541491e4e82bf23ddfbe051d0
Maksim Ekimovskii <ekimovsky.maksim@gmail.com>
2017-10-27 09:26:38 +0300

    async test updates

--- vk_requests/tests/test_session_async.py
+++ vk_requests/tests/test_session_async.py
@@ -1,13 +1,18 @@
+import sys
 import asyncio
 import pytest
 import aiohttp
 
 
-@pytest.mark.asyncio
-async def test_something():
-    session = aiohttp.ClientSession()
-    async with session.get('https://api.github.com/events') as resp:
-        print("resp1: " + str(await resp.json()))
+@pytest.mark.skipIf(sys.version_info < (3, 0))
+class TestAsyncVKSession(object):
+    @pytest.mark.asyncio
+    async def test_something(self):
+        session = aiohttp.ClientSession()
+        async with session.get('https://api.github.com/events') as resp:
+            print("resp1: " + str(await resp.json()))
 
-    async with session.get('https://api.github.com/events') as resp:
-        print("resp2: " + str(await resp.json()))
+        async with session.get('https://api.github.com/events') as resp:
+            print("resp2: " + str(await resp.json()))
+
+        session.close()
